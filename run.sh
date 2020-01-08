5f1b18066c0389a353e080f74c1490bddf7ef505
Vladimir Visnovsky <xvisnov1@fi.muni.cz>
2020-01-08 06:51:10 +0100

    working on converting to BP86

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-docker build -t pipeline:latest .
+#create directories needed for orca docker to pull from information
+mkdir cluster
+mkdir cluster/am1; mkdir cluster/am1/input; mkdir cluster/am1/output
+mkdir cluster/bp86; mkdir cluster/bp86/input; mkdir cluster/bp86/output
 
-docker run -p 8888:8888 pipeline:latest
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
+           -v $HOME/work/magicforcefield-pipeline/cluster:/work \
+           -e WORK=$HOME/work/magicforcefield-pipeline/cluster \
+           --name pipeline \
+           -ti \
+           -p 8888:8888 \
+           pipeline:latest \
+           bash
