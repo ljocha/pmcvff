08c01521f06a7769b9e5ed3eced4f79d915eb1f5
Vladimir Visnovsky <xvisnov1@fi.muni.cz>
2020-02-01 19:28:22 +0100

    fully integrated docker gromacs

--- notebook_run.sh
+++ notebook_run.sh
@@ -3,6 +3,10 @@ mkdir work
 mkdir work/am1; mkdir work/am1/input; mkdir work/am1/output
 mkdir work/bp86; mkdir work/bp86/input; mkdir work/bp86/output
 mkdir work/molekula
+mkdir work/clustering
+mkdir work/clustering/outClustersPDB
+mkdir work/clustering/outClustersXYZ
+mkdir work/clustering/orcaClusters
 cp {tleapin.txt,pipelineJupyter.ipynb} work
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
