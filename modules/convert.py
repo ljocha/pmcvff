beffe7ecd616f9f417a88ce471145989f14b4445
Vladimir Visnovsky <xvisnov1@fi.muni.cz>
2020-02-12 20:10:31 +0100

    added modules

---
+++ modules/convert.py
@@ -0,0 +1,36 @@
+import os
+
+
+def convert_to_orca_methods(input_path, output_path, torsions, method):
+    for xyz_cluster in os.listdir(input_path):
+        if (not xyz_cluster.endswith(".xyz")) or (xyz_cluster.endswith("trj.xyz")):
+            continue
+        output_file = open(output_path + xyz_cluster.replace(".xyz", ".inp"), "w")
+        output_file.write(method + os.linesep)
+        output_file.write("%geom" + os.linesep)
+        output_file.write("Constraints" + os.linesep)
+        write_torsions(output_file, torsions)
+        output_file.write("end" + os.linesep)
+        output_file.write("end" + os.linesep)
+        output_file.write(os.linesep)
+
+        output_file.write("* xyz 1 1" + os.linesep)
+        copy_xyz_to_orca(input_path + xyz_cluster, output_file)
+        output_file.write("*" + os.linesep)
+
+        
+def copy_xyz_to_orca(input_file, output_file):
+    i = 0
+    input_file = open(input_file, "r")
+    for line in input_file:
+        if i >= 2:
+            output_file.write(line)
+        i += 1
+
+
+def write_torsions(output_file, torsionsList):
+    for torsions in torsionsList:
+        output_file.write("{D ")
+        for torsion in torsions:
+            output_file.write(str(torsion) + " ")
+        output_file.write("C}" + os.linesep)
---
+++ modules/execute_orca.py
@@ -0,0 +1,11 @@
+import os
+
+def execute_orca(input_path, output_path):
+    for orca_method in os.listdir(input_path):
+        if not orca_method.endswith(".inp"):
+            continue
+        output_dir_path = output_path + orca_method.replace(".inp", "") + "/"
+        os.mkdir(output_dir_path)
+        command = "cp " + input_path + orca_method + " " + output_dir_path + "; docker run -v $WORK:/work pipeline_orca /opt/orca/orca " + output_dir_path + orca_method
+        os.system(command)
+
