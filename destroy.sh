900caa9ae86235319b702bbdf2aaea5c12b7d7ba
Vladimír Višňovský <467814@mail.muni.cz>
2020-03-06 14:09:52 +0000

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -2,13 +2,6 @@
 
 source config.sh
 
-#container name
-CONTAINER_NAME="pipeline"
-#directory shared between host and container
-SHARED_DIR="work"
-#flag needed to run podman on glados4
-ROOT_TMP="--root=/scratch.ssd/${USER}/tmp"
-
 rm -rf ${SHARED_DIR}
 
 podman stop ${ROOT_TMP} ${CONTAINER_NAME}
