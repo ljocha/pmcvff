88a50f2ad498cfc7b5adde9dffc0855f5cc5ba5d
Vladimir Visnovsky <xvisnov1@glados14.cerit-sc.cz>
2020-09-25 10:17:38 +0200

    tee into stdout as well

--- podman_persist.sh
+++ podman_persist.sh
@@ -1,4 +1,4 @@
-until python3 -u podmand.py >>podmand.log; do
+until python3 -u podmand.py 2>&1 | tee -a podmand.log; do
 	echo "Podman daemon crashed unexpectedly with exit code $?.  Respawning.." >>podmand.log
 	sleep 10
 done
