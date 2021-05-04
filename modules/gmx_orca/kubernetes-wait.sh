371659a3147266aa4d65b8a228122a4f866d848a
Vladimír Višňovský <467814@mail.muni.cz>
2021-05-04 19:29:08 +0200

    reworked build

--- modules/gmx_orca/kubernetes-wait.sh
+++ modules/gmx_orca/kubernetes-wait.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
+#this script is dependant on ttl of watched jobs (wait is completed only after all jobs are deleted)
 
-unset count
 unset label
-while getopts ":l:c:" opt; do
+while getopts ":l:" opt; do
   case $opt in
     l)
       label="$OPTARG"
       echo "Waiting for job(s) with label $label" >&2
-      ;;
-    c)
-      count=$OPTARG
-      echo "Waiting for $OPTARG jobs to complete" >&2
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -23,6 +19,7 @@ while getopts ":l:c:" opt; do
   esac
 done
 
+sleep 3
 while true; do
   succeeded=`kubectl get jobs -l app="$label" -o 'jsonpath={..status.conditions[?(@.type=="Complete")].status}'`
   no_of_succeeded=`echo "$succeeded" | wc -w`
@@ -31,7 +28,7 @@ while true; do
   no_of_failed=`echo "$failed" | wc -w`
 
   summ=$(( no_of_succeeded + no_of_failed ))
-  if [[ summ -eq count ]]; then
+  if [[ summ -eq 0 ]]; then
     break
   fi
 
